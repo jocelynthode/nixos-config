982ca7d90d67e87042f2731584dbae526f1531a2
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-07-08 22:14:50 +0200

    Refactor hardware configuration

--- hosts/desktek/hardware-configuration.nix
+++ hosts/desktek/hardware-configuration.nix
@@ -1,72 +1,14 @@
 { lib, hostname, ... }: {
+  imports = [
+    ../common/optional/btrfs.nix
+  ];
+
   boot = {
     initrd = {
       availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
       kernelModules = [ "kvm-amd" ];
-      postDeviceCommands = lib.mkBefore ''
-        mkdir -p /mnt
-        mount -o subvol=/ /dev/disk/by-label/${hostname} /mnt
-        echo "Cleaning subvolume"
-        btrfs subvolume list -o /mnt/@ | cut -f9 -d ' ' |
-        while read subvolume; do
-          btrfs subvolume delete "/mnt/$subvolume"
-        done && btrfs subvolume delete /mnt/@
-        echo "Restoring blank subvolume"
-        btrfs subvolume snapshot /mnt/@blank /mnt/@
-        umount /mnt
-      '';
-      supportedFilesystems = [ "btrfs" ];
     };
-    resumeDevice = "/dev/disk/by-label/${hostname}";
     kernelParams = [ "resume_offset=22150286" ];
-    kernel.sysctl = {
-      "vm.swappiness" = 10;
-    };
   };
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/${hostname}";
-      fsType = "btrfs";
-      options = [ "defaults,noatime,compress=zstd:1,subvol=@" ];
-    };
-
-    "/var/log" = {
-      device = "/dev/disk/by-label/${hostname}";
-      fsType = "btrfs";
-      options = [ "defaults,noatime,compress=zstd:1,subvol=@log" ];
-      neededForBoot = true;
-    };
-
-    "/nix" = {
-      device = "/dev/disk/by-label/${hostname}";
-      fsType = "btrfs";
-      options = [ "defaults,noatime,compress=zstd:1,subvol=@nix" ];
-    };
-
-    "/persist" = {
-      device = "/dev/disk/by-label/${hostname}";
-      fsType = "btrfs";
-      options = [ "defaults,noatime,compress=zstd:1,subvol=@persist" ];
-      neededForBoot = true;
-    };
-
-    "/swap" = {
-      device = "/dev/disk/by-label/${hostname}";
-      fsType = "btrfs";
-      options = [ "defaults,noatime,compress=zstd:1,subvol=@swap" ];
-    };
-
-    "/boot/efi" = {
-      device = "/dev/disk/by-label/EFI";
-      fsType = "vfat";
-      options = [ "defaults,noatime" ];
-    };
-  };
-
-  swapDevices = [{
-    device = "/swap/swapfile";
-    size = (1024 * 32);
-  }];
 }
 
