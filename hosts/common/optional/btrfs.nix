944bff1d1d50254ceb2684c9e359ead7b68b6d5d
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-07-09 12:51:06 +0200

    Fix boot

--- hosts/common/optional/btrfs.nix
+++ hosts/common/optional/btrfs.nix
@@ -62,7 +62,7 @@
       options = [ "defaults" "noatime" "compress=zstd:1" "subvol=@swap" ];
     };
 
-    "/boot/efi" = {
+    "/boot/EFI" = {
       device = "/dev/disk/by-label/EFI";
       fsType = "vfat";
       options = [ "defaults" "noatime" ];
