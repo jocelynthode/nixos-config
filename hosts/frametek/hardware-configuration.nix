9c6a637128a478ec373cb6b663e9438672e6c536
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-07-09 13:15:20 +0200

    Fix kernel modules

--- hosts/frametek/hardware-configuration.nix
+++ hosts/frametek/hardware-configuration.nix
@@ -6,6 +6,7 @@
 
   boot = {
     initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
       kernelModules = [ "kvm-intel" ];
     };
     # kernelParams = [ "resume_offset=22150286" ];
