aafa3ea687daa65739532be877115b0873714f15
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-07-09 23:45:58 +0200

    Add gammastep indicator

--- home/jocelyn/pkgs/default.nix
+++ home/jocelyn/pkgs/default.nix
@@ -1,4 +1,5 @@
 { pkgs, config }: {
   polybar-bluetooth = pkgs.callPackage ./polybar-bluetooth { config = config; };
+  polybar-gammastep = pkgs.callPackage ./polybar-gammastep { config = config; };
   polybar-mic = pkgs.callPackage ./polybar-mic { config = config; };
 }
---
+++ home/jocelyn/pkgs/polybar-gammastep/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, config }:
+
+pkgs.writeShellApplication {
+  name = "polybar-gammastep";
+  checkPhase = "";
+
+  runtimeInputs = with pkgs; [ coreutils systemd gnugrep findutils ];
+
+  text = ''
+    if unit_status="$(systemctl --user is-active gammastep)"; then
+      status="$unit_status ($(journalctl --user -u gammastep.service | grep 'Period: ' | cut -d ':' -f6 | tail -1 | xargs))"
+      echo "%{F#${config.colorScheme.colors.base0A}}"
+    else
+      status="$unit_status"
+      echo "%{F#${config.colorScheme.colors.base0A}}"
+    fi
+  '';
+}
