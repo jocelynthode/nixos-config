e8ebb335388da642f639dcbfabd82af27ebbaa35
Jocelyn Thode <jocelyn@thode.email>
2022-07-11 17:37:59 +0200

    Fix light path

--- hosts/common/optional/light.nix
+++ hosts/common/optional/light.nix
@@ -3,8 +3,8 @@
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 224 ]; events = [ "key" "rep" ]; command = "/run/current-system/sw/bin/light -U 5"; }
-      { keys = [ 225 ]; events = [ "key" "rep" ]; command = "/run/current-system/sw/bin/light -A 5"; }
+      { keys = [ 224 ]; events = [ "key" "rep" ]; command = "${pkgs.light}/bin/light -U 5"; }
+      { keys = [ 225 ]; events = [ "key" "rep" ]; command = "${pkgs.light}/bin/light -A 5"; }
     ];
   };
 }
