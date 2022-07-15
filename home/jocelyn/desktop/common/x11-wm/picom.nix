285124f19005f3d7a3d9212e61dca8c82aa7e84c
Jocelyn Thode <jocelyn@thode.email>
2022-07-15 15:23:23 +0200

    Update flake

--- home/jocelyn/desktop/common/x11-wm/picom.nix
+++ home/jocelyn/desktop/common/x11-wm/picom.nix
@@ -2,32 +2,32 @@
   services.picom = {
     enable = true;
     experimentalBackends = true;
-    blur = true;
-    vSync = true;
-    activeOpacity = "1.0";
-    inactiveOpacity = "1.0";
-    backend = "glx";
-    fade = false;
-    opacityRule = [
-      "100:fullscreen"
-      "100:name *= 'i3lock'"
-      "85:class_g = 'Spotify'"
-      "85:class_g *?= 'Rofi'"
-    ];
-
-    shadow = true;
-    shadowOpacity = "0.65";
-    shadowExclude = [
-      "name = 'Notification'"
-    ];
-    extraOptions = ''
-      blur-method = "dual_kawase";
-      blur-strength = 5;
-      blur-kern = "3x3box";
+    settings = {
+      blur = {
+        method = "dual_kawase";
+        strength = 5;
+        kern = "3x3box";
+      };
+      vsync = true;
+      active-opacity = "1.0";
+      inactive-opacity = "1.0";
+      backend = "glx";
+      fade = false;
+      opacity-rule = [
+        "100:fullscreen"
+        "100:name *= 'i3lock'"
+        "85:class_g = 'Spotify'"
+        "85:class_g *?= 'Rofi'"
+      ];
+      shadow = true;
+      shadow-opacity = "0.65";
+      shadow-exclude = [
+        "name = 'Notification'"
+      ];
       mark-wmwin-focused = true;
       mark-ovredir-focused = true;
       detect-rounded-corners = true;
       detect-client-opacity = true;
-    '';
+    };
   };
 }
