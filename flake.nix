0e1ca45866f5ffcc0177b811e1f9913a561e5788
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-07-17 22:48:43 +0200

    Switch back to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # TODO switch back when pr on grammars is merged
-    nixpkgs.url = "github:jocelynthode/nixpkgs/with-fixes";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
