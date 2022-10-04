ee4f9ceb6d048fcaba401f8ef98c32b4a536abc3
Jocelyn Thode <jocelyn@thode.email>
2022-10-04 22:30:46 +0200

    Switch to gpg ssh

--- hosts/common/optional/gnome-keyring.nix
+++ hosts/common/optional/gnome-keyring.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }: {
-  security.pam.services = {
-    login.enableGnomeKeyring = true;
-    lightdm.enableGnomeKeyring = true;
-  };
-  environment.systemPackages = [ pkgs.gnome.gnome-keyring pkgs.pinentry-gnome ];
-  services.dbus.packages = with pkgs; [ gnome.gnome-keyring gcr ];
+  # security.pam.services = {
+  #   login.enableGnomeKeyring = true;
+  #   lightdm.enableGnomeKeyring = true;
+  # };
+  environment.systemPackages = with pkgs; [ pinentry-gnome ];
+  services.dbus.packages = with pkgs; [ gcr ];
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      gnome.gnome-keyring
+      # gnome.gnome-keyring
     ];
   };
 }
