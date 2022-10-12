bbb9f9eb4076ed228175b625da99388c594e6b5c
Jocelyn Thode <jocelyn@thode.email>
2022-10-12 18:45:24 +0200

    Add taskwarrior client config

--- modules/programs/taskwarrior/default.nix
+++ modules/programs/taskwarrior/default.nix
@@ -5,6 +5,24 @@
   };
 
   config = lib.mkIf config.aspects.programs.taskwarrior.enable {
+    sops.secrets."taskwarrior/certificate" = {
+      sopsFile = ../../../secrets/common/secrets.yaml;
+      owner = "jocelyn";
+      group = "users";
+    };
+
+    sops.secrets."taskwarrior/key" = {
+      sopsFile = ../../../secrets/common/secrets.yaml;
+      owner = "jocelyn";
+      group = "users";
+    };
+
+    sops.secrets."taskwarrior/ca" = {
+      sopsFile = ../../../secrets/common/secrets.yaml;
+      owner = "jocelyn";
+      group = "users";
+    };
+
     home-manager.users.jocelyn = { ... }: {
       programs.taskwarrior = {
         enable = true;
@@ -13,6 +31,48 @@
           context.work.write = "+work";
           context.home.read = "+home";
           context.home.write = "+home";
+          taskd.certificate = config.sops.secrets."taskwarrior/certificate".path;
+          taskd.key = config.sops.secrets."taskwarrior/key".path;
+          taskd.ca = config.sops.secrets."taskwarrior/ca".path;
+          taskd.credentials = "public/jocelyn/98c8e2b7-67e1-4680-a89d-4d562efc9f66";
+          taskd.server = "tasks.tekila.ovh:53589";
+        };
+      };
+
+      systemd.user = {
+        services.taskwarrior-sync = {
+          Unit = {
+            Description = "Run task sync periodically";
+            After = [
+              "network-online.target"
+            ];
+            Wants = [
+              "taskwarrior-sync.timer"
+              "timers.target"
+            ];
+          };
+          Service = {
+            Type = "oneshot";
+            ExecStart = "${pkgs.taskwarrior}/bin/task sync";
+          };
+        };
+        timers.taskwarrior-sync = {
+          Unit = {
+            Description = "Run task sync";
+            After = [
+              "network-online.target"
+            ];
+            Requires = [
+              "taskwarrior-sync.service"
+            ];
+          };
+          Timer = {
+            Unit = "taskwarrior-sync.service";
+            OnCalendar = "*:0/15";
+          };
+          Install = {
+            WantedBy = [ "timers.target" ];
+          };
         };
       };
     };
