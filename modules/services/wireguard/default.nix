2ddca4c5e45ddefa11854719e48f125c17d607ff
Jocelyn Thode <jocelyn@thode.email>
2022-10-15 11:38:43 +0200

    Update wireguard

--- modules/services/wireguard/default.nix
+++ modules/services/wireguard/default.nix
@@ -8,22 +8,18 @@
     networking.firewall = {
       allowedUDPPorts = [ 51820 ];
     };
-    networking.wireguard = {
-      enable = true;
-      interfaces = {
-        wg0 = {
-          ips = [ "10.2.0.2/24" ];
-          listenPort = 51820;
-          privateKeyFile = config.sops.secrets.wireguard.path;
-          peers = [
-            {
-              publicKey = "VNNO5MYorFu1UerHvoXccW6TvotxbJ1GAGJKtzM9HTY=";
-              allowedIPs = [ "0.0.0.0/0" ];
-              endpoint = "185.159.157.23:51820";
-              persistentKeepalive = 25;
-            }
-          ];
-        };
+    networking.wg-quick.interfaces = {
+      wg0 = {
+        address = [ "10.2.0.2/32" ];
+        dns = [ "10.2.0.1" ];
+        privateKeyFile = config.sops.secrets.wireguard.path;
+        peers = [
+          {
+            publicKey = "VNNO5MYorFu1UerHvoXccW6TvotxbJ1GAGJKtzM9HTY=";
+            allowedIPs = [ "0.0.0.0/0" ];
+            endpoint = "185.159.157.23:51820";
+          }
+        ];
       };
     };
 
