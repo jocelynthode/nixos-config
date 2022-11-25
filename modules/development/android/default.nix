93a88bb796e416d98af6f6a6fbd1aa1de870baf4
Jocelyn Thode <jocelyn@thode.email>
2022-11-25 15:36:18 +0100

    Add android folder

--- modules/development/android/default.nix
+++ modules/development/android/default.nix
@@ -10,6 +10,10 @@
   };
 
   config = lib.mkIf config.aspects.development.android.enable {
+    aspects.base.persistence.homePaths = [
+      ".android"
+    ];
+
     programs.adb.enable = true;
     users.users.jocelyn.extraGroups = ["adbusers"];
     services.udev.packages = with pkgs; [
