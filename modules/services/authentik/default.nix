932f5e5aec4a0c2c01eff02a123eea630b2da255
Jocelyn Thode <jocelyn@thode.email>
2022-11-26 16:11:42 +0100

    Fix authentik config

--- modules/services/authentik/default.nix
+++ modules/services/authentik/default.nix
@@ -1,39 +1,51 @@
-{config, ...}: let
-  definition = _command: {
-    image = "ghcr.io/goauthentik/server:2022.11.1";
-    cmd = ["server"];
-    dependsOn = [
-    ];
-    extraOptions = [
-      "--network=host"
-      "--pull=always"
-    ];
-    volumes = [
-    ];
-    environment = {
-      AUTHENTIK_REDIS__HOST = "127.0.0.1";
-      AUTHENTIK_REDIS__PORT = "6379";
-      AUTHENTIK_REDIS__DB = "0";
-      AUTHENTIK_POSTGRESQL__HOST = "127.0.0.1";
-      AUTHENTIK_POSTGRESQL__USER = "authentik";
-      AUTHENTIK_POSTGRESQL__NAME = "authentik";
-      AUTHENTIK_COOKIE_DOMAIN = ".tekila.ovh";
-    };
-    environmentFiles = [
-      config.sops.secrets.authentik.path
-    ];
-  };
-in {
-  virtualisation.oci-containers.containers = {
-    authentik-server = definition "server";
-    authentik-worker = definition "worker";
+{
+  config,
+  lib,
+  ...
+}: {
+  options.aspects.services.authentik.enable = lib.mkOption {
+    default = false;
+    example = true;
   };
 
-  sops.secrets.authentik = {
-    sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
-    restartUnits = [
-      "podman-authentik-server.service"
-      "podman-authentik-worker.service"
-    ];
-  };
+  config = let
+    definition = _command: {
+      image = "ghcr.io/goauthentik/server:2022.11.1";
+      cmd = ["server"];
+      dependsOn = [
+      ];
+      extraOptions = [
+        "--network=host"
+        "--pull=always"
+      ];
+      volumes = [
+      ];
+      environment = {
+        AUTHENTIK_REDIS__HOST = "127.0.0.1";
+        AUTHENTIK_REDIS__PORT = "6379";
+        AUTHENTIK_REDIS__DB = "0";
+        AUTHENTIK_POSTGRESQL__HOST = "127.0.0.1";
+        AUTHENTIK_POSTGRESQL__USER = "authentik";
+        AUTHENTIK_POSTGRESQL__NAME = "authentik";
+        AUTHENTIK_COOKIE_DOMAIN = ".tekila.ovh";
+      };
+      environmentFiles = [
+        config.sops.secrets.authentik.path
+      ];
+    };
+  in
+    lib.mkIf config.aspects.services.authentik.enable {
+      virtualisation.oci-containers.containers = {
+        authentik-server = definition "server";
+        authentik-worker = definition "worker";
+      };
+
+      sops.secrets.authentik = {
+        sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
+        restartUnits = [
+          "podman-authentik-server.service"
+          "podman-authentik-worker.service"
+        ];
+      };
+    };
 }
