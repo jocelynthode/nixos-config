03eba08a24bfa4cd69edb81da6231d67d00e5d07
Jocelyn Thode <jocelyn@thode.email>
2022-12-24 10:16:57 +0100

    Update flake

--- modules/programs/obs-studio/default.nix
+++ modules/programs/obs-studio/default.nix
@@ -18,7 +18,7 @@
       programs.obs-studio = {
         enable = true;
         plugins = with pkgs.obs-studio-plugins; [
-          obs-backgroundremoval
+          # obs-backgroundremoval
           obs-pipewire-audio-capture
         ];
       };
