38cf29f23924c627df70026ac05a57d1e58842de
Jocelyn Thode <jocelyn@thode.email>
2023-01-04 23:00:59 +0100

    Update authentik to 2022.12.1

--- modules/services/authentik/default.nix
+++ modules/services/authentik/default.nix
@@ -15,7 +15,7 @@
       volumes,
     }: {
       inherit cmd volumes dependsOn;
-      image = "ghcr.io/goauthentik/server:2022.12.0";
+      image = "ghcr.io/goauthentik/server:2022.12.1";
       user = "100000:100000"; # authentik:authentik
       extraOptions = [
         "--network=host"
