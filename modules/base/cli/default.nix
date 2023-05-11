0621cd4f3e17044e35ac6b4fc25ea8ea26f9ecd3
Jocelyn Thode <jocelyn@thode.email>
2023-05-11 08:21:43 +0200

    Fix lsd theme

--- modules/base/cli/default.nix
+++ modules/base/cli/default.nix
@@ -25,8 +25,43 @@
         enable = true;
         settings = {
           symlink-arrow = "⇒";
+          color = {
+            theme = "base16";
+          };
         };
       };
+    };
+
+    xdg.configFile."lsd/themes/base16.yaml" = {
+      text = ''
+        user: dark_yellow
+        group: dark_yellow
+        permission:
+          read: dark_green
+          write: dark_yellow
+          exec: dark_red
+          exec-sticky: dark_blue
+          no-access: black
+          octal: dark_blue
+          acl: dark_cyan
+          context: cyan
+        date:
+          hour-old: green
+          day-old: green
+          older: green
+        size:
+          none: magenta
+          small: magenta
+          medium: magenta
+          large: magenta
+        inode:
+          valid: 13
+          invalid: 245
+        links:
+          valid: dark_green
+          invalid: dark_red
+        tree-edge: 245
+      '';
     };
   };
 in {
