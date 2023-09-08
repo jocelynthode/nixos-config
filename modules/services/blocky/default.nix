0572dec8a1b8c312681d13ac99627a136ea3e344
Jocelyn Thode <jocelyn@thode.email>
2023-09-08 10:12:57 +0200

    Fix upstreams

--- modules/services/blocky/default.nix
+++ modules/services/blocky/default.nix
@@ -23,12 +23,14 @@
           dns = 53;
           https = 4443;
         };
-        upstream.default = [
-          "https://dns10.quad9.net/dns-query"
-          "https://dns.mullvad.net/dns-query"
-          "https://one.one.one.one/dns-query"
-          "https://dns.google/dns-query"
-        ];
+        upstream.groups = {
+          default = [
+            "https://dns10.quad9.net/dns-query"
+            "https://dns.mullvad.net/dns-query"
+            "https://one.one.one.one/dns-query"
+            "https://dns.google/dns-query"
+          ];
+        };
         # For initially solving DoH/DoT Requests when no system Resolver is available.
         bootstrapDns = {
           upstream = "https://dns.quad9.net/dns-query";
