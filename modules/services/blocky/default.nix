3b9786b8edeb8d1a88b758e4c173bf7f466f8875
Jocelyn Thode <jocelyn@thode.email>
2023-09-18 22:26:45 +0200

    Add whitelist for youtube history

--- modules/services/blocky/default.nix
+++ modules/services/blocky/default.nix
@@ -9,62 +9,63 @@
     example = true;
   };
 
-  config = lib.mkIf config.aspects.services.blocky.enable rec {
-    networking = {
-      firewall = {
-        allowedTCPPorts = [53 853];
-        allowedUDPPorts = [53];
-      };
-    };
-
+  config = let
     whitelist =
       pkgs.writeText "whitelist.txt"
       ''
         s.youtube.com
       '';
+  in
+    lib.mkIf config.aspects.services.blocky.enable rec {
+      networking = {
+        firewall = {
+          allowedTCPPorts = [53 853];
+          allowedUDPPorts = [53];
+        };
+      };
 
-    services.blocky = {
-      enable = true;
-      settings = {
-        ports = {
-          dns = 53;
-          https = 4443;
-          tls = 8853;
-        };
-        upstreams.groups = {
-          default = [
-            "https://dns10.quad9.net/dns-query"
-            "https://dns.mullvad.net/dns-query"
-            "https://one.one.one.one/dns-query"
-            "https://dns.google/dns-query"
-          ];
-        };
-        # For initially solving DoH/DoT Requests when no system Resolver is available.
-        bootstrapDns = {
-          upstream = "https://dns.quad9.net/dns-query";
-          ips = ["9.9.9.9" "149.112.112.112"];
-        };
-        blocking = {
-          blackLists = {
-            ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+      services.blocky = {
+        enable = true;
+        settings = {
+          ports = {
+            dns = 53;
+            https = 4443;
+            tls = 8853;
           };
-          whitelists = {
-            ads = ["${whitelist}"];
+          upstreams.groups = {
+            default = [
+              "https://dns10.quad9.net/dns-query"
+              "https://dns.mullvad.net/dns-query"
+              "https://one.one.one.one/dns-query"
+              "https://dns.google/dns-query"
+            ];
           };
-          clientGroupsBlock = {
-            default = ["ads"];
+          # For initially solving DoH/DoT Requests when no system Resolver is available.
+          bootstrapDns = {
+            upstream = "https://dns.quad9.net/dns-query";
+            ips = ["9.9.9.9" "149.112.112.112"];
           };
-        };
-        log = {
-          level = "warn";
-          privacy = true;
-        };
-        caching = {
-          minTime = "5m";
-          maxTime = "30m";
-          prefetching = true;
+          blocking = {
+            blackLists = {
+              ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+            };
+            whiteLists = {
+              ads = ["${whitelist}"];
+            };
+            clientGroupsBlock = {
+              default = ["ads"];
+            };
+          };
+          log = {
+            level = "warn";
+            privacy = true;
+          };
+          caching = {
+            minTime = "5m";
+            maxTime = "30m";
+            prefetching = true;
+          };
         };
       };
     };
-  };
 }
