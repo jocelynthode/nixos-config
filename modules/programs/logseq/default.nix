a5cb3509b3dfdf401340381daa307163b5a68c95
Jocelyn Thode <jocelyn@thode.email>
2024-07-06 13:52:18 +0200

    Udate flake.lock

--- modules/programs/logseq/default.nix
+++ modules/programs/logseq/default.nix
@@ -12,6 +12,7 @@
   config = lib.mkIf config.aspects.programs.logseq.enable {
     nixpkgs.config.permittedInsecurePackages = [
       "electron-28.3.3"
+      "electron-27.3.11"
     ];
 
     environment.systemPackages = with pkgs; [
