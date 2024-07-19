7c9910327a822f601117fbdc74cdbf1d0f94d74c
Jocelyn Thode <jocelyn@thode.email>
2024-07-19 10:26:19 +0200

    config home assistant

--- modules/services/home-assistant/default.nix
+++ modules/services/home-assistant/default.nix
@@ -31,44 +31,11 @@
     services.home-assistant = {
       enable = true;
       openFirewall = true;
-      package =
-        (pkgs.home-assistant.override {
-          extraPackages = py: with py; [psycopg2];
-        })
-        .overrideAttrs (_oldAttrs: {
-          doInstallCheck = false;
-        });
+      package = pkgs.home-assistant.override {extraPackages = ps: [ps.psycopg2];};
       config.recorder.db_url = "postgresql://@/hass";
-      defaultComponents = [
-        "application_credentials"
-        "frontend"
-        "hardware"
-        "logger"
-        "network"
-        "system_health"
-        "automation"
-        "person"
-        "scene"
-        "script"
-        "tag"
-        "zone"
-        "counter"
-        "input_boolean"
-        "input_button"
-        "input_datetime"
-        "input_number"
-        "input_select"
-        "input_text"
-        "schedule"
-        "timer"
-        "backup"
-        "esphome"
-      ];
       extraComponents = [
         # Components required to complete the onboarding
         "esphome"
-        "met"
-        "map"
       ];
       config = {
         # Includes dependencies for a basic setup
