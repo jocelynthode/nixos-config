828969e04ef2a7e80fe7a84f119cd97e42ee9a64
Jocelyn Thode <jocelyn@thode.email>
2024-07-22 19:03:58 +0200

    Add esphome

--- modules/services/home-assistant/default.nix
+++ modules/services/home-assistant/default.nix
@@ -13,54 +13,67 @@
     aspects.base.persistence.systemPaths = [
       {
         directory = "/var/lib/hass";
+        user = "hass";
+        group = "hass";
+      }
+      {
+        directory = "/var/lib/esphome";
+        user = "esphome";
+        group = "esphome";
       }
     ];
 
-    services.postgresql = {
-      ensureDatabases = [
-        "hass"
-      ];
-      ensureUsers = [
-        {
-          name = "hass";
-          ensureDBOwnership = true;
-        }
-      ];
-    };
-
-    services.home-assistant = {
-      enable = true;
-      openFirewall = true;
-      package = pkgs.home-assistant.override {extraPackages = ps: [ps.psycopg2];};
-      extraComponents = [
-        # Components required to complete the onboarding
-        "esphome"
-        "co2signal"
-        "twinkly"
-        "utility_meter"
-        "zha"
-      ];
-      customComponents = with pkgs.home-assistant-custom-components; [
-        auth-header
-      ];
-      config = {
-        recorder = {
-          db_url = "postgresql://@/hass";
-          purge_keep_days = 30;
-        };
-        # https://www.home-assistant.io/integrations/default_config/
-        default_config = {};
-        api = {};
-        "automation ui" = "!include automations.yaml";
-        http = {
-          use_x_forwarded_for = true;
-          trusted_proxies = [
-            "127.0.0.1"
-            "::1"
-          ];
-        };
-        auth_header = {
-          username_header = "X-authentik-username";
+    services = {
+      esphome = {
+        enable = true;
+        address = "0.0.0.0";
+        openFirewall = true;
+      };
+      postgresql = {
+        ensureDatabases = [
+          "hass"
+        ];
+        ensureUsers = [
+          {
+            name = "hass";
+            ensureDBOwnership = true;
+          }
+        ];
+      };
+      home-assistant = {
+        enable = true;
+        openFirewall = true;
+        package = pkgs.home-assistant.override {extraPackages = ps: [ps.psycopg2];};
+        extraComponents = [
+          # Components required to complete the onboarding
+          "esphome"
+          "co2signal"
+          "twinkly"
+          "utility_meter"
+          "zha"
+        ];
+        customComponents = with pkgs.home-assistant-custom-components; [
+          auth-header
+        ];
+        config = {
+          recorder = {
+            db_url = "postgresql://@/hass";
+            purge_keep_days = 30;
+          };
+          # https://www.home-assistant.io/integrations/default_config/
+          default_config = {};
+          api = {};
+          "automation ui" = "!include automations.yaml";
+          http = {
+            use_x_forwarded_for = true;
+            trusted_proxies = [
+              "127.0.0.1"
+              "::1"
+            ];
+          };
+          auth_header = {
+            username_header = "X-authentik-username";
+          };
         };
       };
     };
