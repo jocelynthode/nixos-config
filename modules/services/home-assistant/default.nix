2576df22777db757a0bf4eada31574ae9bc41766
Jocelyn Thode <jocelyn@thode.email>
2024-07-24 16:48:25 +0200

    Add z2m setup

--- modules/services/home-assistant/default.nix
+++ modules/services/home-assistant/default.nix
@@ -16,6 +16,12 @@
         user = "hass";
         group = "hass";
       }
+      {
+        directory = "/var/lib/zigbee2mqtt";
+      }
+      {
+        directory = "/var/lib/mosquitto";
+      }
     ];
 
     services = {
@@ -23,6 +29,50 @@
         enable = true;
         address = "0.0.0.0";
         openFirewall = true;
+      };
+      mosquitto = {
+        enable = true;
+        settings = {
+          allow_anonymous = false;
+        };
+        listeners = [
+          {
+            address = "127.0.0.1";
+            acl = ["topic readwrite #"];
+
+            users.hass = {
+              acl = [
+                "readwrite #"
+              ];
+              passwordFile = config.sops.secrets."mqtt/hass".path;
+            };
+
+            users."${config.services.zigbee2mqtt.settings.mqtt.user}" = {
+              acl = [
+                "readwrite #"
+              ];
+              passwordFile = config.sops.secrets."mqtt/zigbee2mqtt".path;
+            };
+          }
+        ];
+      };
+      zigbee2mqtt = {
+        enable = true;
+        settings = {
+          homeassistant = config.services.home-assistant.enable;
+          permit_join = false;
+          mqtt = {
+            base_topic = "zigbee2mqtt";
+            server = "mqtt://localhost:1883";
+            user = "zigbee2mqtt";
+            password = "!secret password";
+          };
+          serial = {
+            port = "/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_0222f2dc9bb3ed11977243aca7669f5d-if00-port0";
+            adapter = "ember";
+            rtscts = true;
+          };
+        };
       };
       postgresql = {
         ensureDatabases = [
@@ -40,19 +90,18 @@
         openFirewall = true;
         package = pkgs.home-assistant.override {extraPackages = ps: [ps.psycopg2];};
         extraComponents = [
-          # Components required to complete the onboarding
-          "esphome"
-          "co2signal"
-          "twinkly"
-          "utility_meter"
-          "zha"
-          "kodi"
-          "met"
           "brother"
+          "co2signal"
           "dlna_dmr"
-          "upnp"
+          "esphome"
           "ipp"
           "jellyfin"
+          "kodi"
+          "met"
+          "twinkly"
+          "upnp"
+          "utility_meter"
+          "mqtt"
         ];
         customComponents = with pkgs.home-assistant-custom-components; [
           auth-header
@@ -80,11 +129,30 @@
       };
     };
 
-    sops.secrets.home-assistant = {
-      sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
-      owner = "hass";
-      path = "/var/lib/hass/secrets.yaml";
-      restartUnits = ["home-assistant.service"];
+    sops.secrets = {
+      "mqtt/hass" = {
+        sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
+        owner = "mosquitto";
+        restartUnits = ["mosquitto.service"];
+      };
+      "mqtt/zigbee2mqtt" = {
+        sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
+        owner = "mosquitto";
+        restartUnits = ["mosquitto.service"];
+      };
+      "zigbee2mqtt/password" = {
+        sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
+        path = "/var/lib/zigbee2mqtt/secret.yaml";
+        owner = "${config.systemd.services.zigbee2mqtt.serviceConfig.User}";
+        group = "${config.systemd.services.zigbee2mqtt.serviceConfig.Group}";
+        restartUnits = ["zigbee2mqtt.service"];
+      };
+      home-assistant = {
+        sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
+        owner = "hass";
+        path = "/var/lib/hass/secrets.yaml";
+        restartUnits = ["home-assistant.service"];
+      };
     };
   };
 }
