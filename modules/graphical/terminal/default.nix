6ba530037c46d2b8e482ef42e83f31b24446ff01
Jocelyn Thode <jocelyn@thode.email>
2024-08-23 12:38:32 +0200

    Add kitty shortcuts

--- modules/graphical/terminal/default.nix
+++ modules/graphical/terminal/default.nix
@@ -22,6 +22,19 @@
         enable = true;
         font.name = osConfig.aspects.base.fonts.monospace.family;
         font.size = osConfig.aspects.base.fonts.monospace.size;
+        keybindings = {
+          "alt+tab" = "select_tab";
+          "alt+1" = "goto_tab 1";
+          "alt+2" = "goto_tab 2";
+          "alt+3" = "goto_tab 3";
+          "alt+4" = "goto_tab 4";
+          "alt+5" = "goto_tab 5";
+          "alt+6" = "goto_tab 6";
+          "alt+7" = "goto_tab 7";
+          "alt+8" = "goto_tab 8";
+          "alt+9" = "goto_tab 9";
+          "alt+0" = "goto_tab 10";
+        };
         settings = {
           allow_remote_control = "yes";
 
