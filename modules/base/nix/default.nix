7814c5a9a2fa7a3345ee59b13acdbaf1127572b7
Jocelyn Thode <jocelyn@thode.email>
2024-11-01 13:25:05 +0100

    Add garnix

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -46,12 +46,14 @@ in {
           "https://nix-community.cachix.org"
           "https://tekila.cachix.org"
           "https://devenv.cachix.org"
+          "https://cache.garnix.io"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "tekila.cachix.org-1:Ujkoh3GxcP2pnxmUzMPqBasUVmnI61TUry0VaL0uD68="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         ];
 
         trusted-users = ["root" "@wheel"];
