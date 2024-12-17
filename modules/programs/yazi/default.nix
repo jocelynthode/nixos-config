a3bfb4f5d94078a952396334c3a8039e54c16a97
Jocelyn Thode <jocelyn@thode.email>
2024-12-17 21:16:28 +0100

    Fix easyeffects

--- modules/programs/yazi/default.nix
+++ modules/programs/yazi/default.nix
@@ -27,7 +27,7 @@
         settings = {
           manager = {
             show_hidden = true;
-            sort_by = "modified";
+            sort_by = "mtime";
             sort_dir_first = true;
             sort_reverse = true;
           };
