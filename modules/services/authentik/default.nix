b319010bfef0697e6d4c9185fec51bf98ef54a0e
Jocelyn Thode <jocelyn@thode.email>
2025-01-03 13:25:29 +0100

    Update authentik to 2024.12.1

--- modules/services/authentik/default.nix
+++ modules/services/authentik/default.nix
@@ -15,7 +15,7 @@
       volumes,
     }: {
       inherit cmd volumes dependsOn;
-      image = "ghcr.io/goauthentik/server:2024.10.0";
+      image = "ghcr.io/goauthentik/server:2024.12.1";
       user = "100000:100000"; # authentik:authentik
       extraOptions = [
         "--network=host"
