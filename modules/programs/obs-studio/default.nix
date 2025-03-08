3d39ce9d05f88bec959321b33a648828f5d6b660
Jocelyn Thode <jocelyn@thode.email>
2025-03-08 10:55:50 +0100

    Fix obs

--- modules/programs/obs-studio/default.nix
+++ modules/programs/obs-studio/default.nix
@@ -29,11 +29,15 @@
       catppuccin.obs.enable = true;
       programs.obs-studio = {
         enable = true;
+        package = pkgs.obs-studio.override {
+          browserSupport = true;
+        };
         plugins = with pkgs.obs-studio-plugins; [
           # obs-backgroundremoval
           obs-pipewire-audio-capture
           wlrobs
           obs-vkcapture
+          obs-composite-blur
         ];
       };
     };
