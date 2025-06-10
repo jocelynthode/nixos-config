142d0753df6526bc0d9ef8bee94325cfccacc79b
Jocelyn Thode <jocelyn@thode.email>
2025-06-10 14:58:48 +0200

    Fix hibernate

--- modules/graphical/wleave/default.nix
+++ modules/graphical/wleave/default.nix
@@ -13,44 +13,46 @@
       programs.wlogout = {
         enable = true;
         package = pkgs.wleave;
-        layout = [
+        layout =
+          [
+            {
+              label = "lock";
+              action = "loginctl lock-session";
+              text = "Lock";
+              keybind = "l";
+            }
+            {
+              label = "logout";
+              action = "loginctl terminate-user $USER";
+              text = "Logout";
+              keybind = "e";
+            }
+            {
+              label = "shutdown";
+              action = "systemctl poweroff";
+              text = "Shutdown";
+              keybind = "s";
+            }
+            {
+              label = "suspend";
+              action = "systemctl suspend";
+              text = "Suspend";
+              keybind = "u";
+            }
+            {
+              label = "reboot";
+              action = "systemctl reboot";
+              text = "Reboot";
+              keybind = "r";
+            }
+          ]
+          ++ lib.optional (builtins.length config.swapDevices > 0)
           {
-            label = "lock";
-            action = "loginctl lock-session";
-            text = "Lock";
-            keybind = "l";
-          }
-          # {
-          #   label = "hibernate";
-          #   action = "systemctl hibernate";
-          #   text = "Hibernate";
-          #   keybind = "h";
-          # }
-          {
-            label = "logout";
-            action = "loginctl terminate-user $USER";
-            text = "Logout";
-            keybind = "e";
-          }
-          {
-            label = "shutdown";
-            action = "systemctl poweroff";
-            text = "Shutdown";
-            keybind = "s";
-          }
-          {
-            label = "suspend";
-            action = "systemctl suspend";
-            text = "Suspend";
-            keybind = "u";
-          }
-          {
-            label = "reboot";
-            action = "systemctl reboot";
-            text = "Reboot";
-            keybind = "r";
-          }
-        ];
+            label = "hibernate";
+            action = "systemctl hibernate";
+            text = "Hibernate";
+            keybind = "h";
+          };
       };
     };
   };
