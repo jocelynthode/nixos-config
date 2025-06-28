5eca932b6da59858dc251e8af24e080f19ede842
Jocelyn Thode <jocelyn@thode.email>
2025-06-28 10:19:27 +0200

    Remove network-online patch

--- modules/base/network/default.nix
+++ modules/base/network/default.nix
@@ -35,9 +35,6 @@
     };
   };
 
-  # See https://github.com/NixOS/nixpkgs/commit/15d761a525a025de0680b62e8ab79a9d183f313d
-  systemd.targets.network-online.wantedBy = lib.mkForce []; # Normally ["multi-user.target"]
-  systemd.services.NetworkManager-wait-online.wantedBy = lib.mkForce []; # Normally ["network-online.target"]
   users.users.jocelyn.extraGroups = lib.mkIf config.networking.networkmanager.enable ["networkmanager"];
 
   home-manager.users.jocelyn = {pkgs, ...}: {
