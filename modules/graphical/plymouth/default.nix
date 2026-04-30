7f9d61aaa0d5fbff473b3d0657cb29d8ad141874
Jocelyn Thode <jocelyn@thode.email>
2026-04-30 08:36:40 +0200

    Add plymouth temp fix

--- modules/graphical/plymouth/default.nix
+++ modules/graphical/plymouth/default.nix
@@ -11,6 +11,12 @@
 
     boot = {
       plymouth.enable = true;
+      # TODO https://discourse.nixos.org/t/i-found-a-fix-for-plymouth-not-displaying-but-im-not-sure-who-to-tell/77247?u=waffle8946 while it is fixed
+      # https://github.com/NixOS/nixpkgs/issues/26722#issuecomment-4345848717
+      initrd.systemd.services.plymouth-start = {
+        after = [ "systemd-modules-load.service" ];
+        requires = [ "systemd-modules-load.service" ];
+      };
       kernelParams = [
         "quiet"
         "splash"
