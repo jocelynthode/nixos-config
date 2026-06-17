69c13da7a0930742351c80c3ca25f47dbcc874a3
Jocelyn Thode <jocelyn@thode.email>
2026-06-17 15:58:39 +0200

    Fix swayidle

--- modules/graphical/swayidle/default.nix
+++ modules/graphical/swayidle/default.nix
@@ -1,53 +1,34 @@
 {
-  pkgs,
   config,
   lib,
   ...
 }:
 {
   config = lib.mkIf config.aspects.graphical.enable {
-    home-manager.users.jocelyn =
-      {
-        osConfig,
-        config,
-        ...
-      }:
-      {
-        services.swayidle = {
-          enable = true;
-          systemdTargets = [ "graphical-session.target" ];
-          events = {
-            "before-sleep" = "${lib.getExe config.programs.noctalia.package} msg session lock";
-            "lock" = "${lib.getExe config.programs.noctalia.package} msg session lock";
-          };
-          timeouts = [
-            {
-              timeout = 600;
-              command = "${lib.getExe config.programs.noctalia.package} msg session lock";
-            }
-            {
-              timeout = 610;
-              command = "${lib.getExe config.programs.noctalia.package} msg mic-mute";
-            }
-            {
-              timeout = 700;
-              command =
-                if osConfig.aspects.graphical.hyprland.enable then
-                  "hyprctl dispatch dpms off"
-                else if osConfig.aspects.graphical.niri.enable then
-                  "${lib.getExe config.programs.niri.package} msg dpms-off"
-                else
-                  "${pkgs.sway}/bin/swaymsg \"output * dpms off\"";
-              resumeCommand =
-                if osConfig.aspects.graphical.hyprland.enable then
-                  "hyprctl dispatch dpms on"
-                else if osConfig.aspects.graphical.niri.enable then
-                  "${lib.getExe config.programs.niri.package} msg dpms-on"
-                else
-                  "${pkgs.sway}/bin/swaymsg \"output * dpms on\"";
-            }
-          ];
+    home-manager.users.jocelyn = { config, ... }: {
+      services.swayidle = {
+        enable = true;
+        systemdTargets = [ "graphical-session.target" ];
+        events = {
+          "before-sleep" = "${lib.getExe config.programs.noctalia.package} msg session lock";
+          "lock" = "${lib.getExe config.programs.noctalia.package} msg session lock";
         };
+        timeouts = [
+          {
+            timeout = 600;
+            command = "${lib.getExe config.programs.noctalia.package} msg session lock";
+          }
+          {
+            timeout = 610;
+            command = "${lib.getExe config.programs.noctalia.package} msg mic-mute";
+          }
+          {
+            timeout = 700;
+            command = "${lib.getExe config.programs.noctalia.package} msg dpms-off";
+            resumeCommand = "${lib.getExe config.programs.noctalia.package} msg dpms-on";
+          }
+        ];
       };
+    };
   };
 }
