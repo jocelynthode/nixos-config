798f5f8ee18c2cf20455502a848b8ad8e748744f
Jocelyn Thode <jocelyn@thode.email>
2026-08-20 20:44:01 +0200

    Use new version for groupe-e

--- pkgs/home-assistant-custom-components/groupe-e/default.nix
+++ pkgs/home-assistant-custom-components/groupe-e/default.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent {
   owner = "jocelynthode";
   domain = "groupe_e";
-  version = "1.0.14";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "jocelynthode";
     repo = "hscs-groupe-e";
-    rev = "9549448f5a161e1c16eb5ca366f00e2a952968ee";
-    hash = "sha256-0B9WEQ4rAwzFJhsO+vhOjE32SwDWPbO8Y0Wcp3eZmQ8=";
+    rev = "92cd024dd4319db38a789d3029578ada428c063c";
+    hash = "sha256-5vCb6aR2UX4gBQVj7PMgXbtNORAsmM3ytBhe6HV3fRY=";
   };
 
   dependencies = with python3Packages; [ aiohttp ];
