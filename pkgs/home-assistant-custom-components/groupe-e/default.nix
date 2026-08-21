e9a201c275fa9cf374ae23737aa3978a339ddd6b
Jocelyn Thode <jocelyn@thode.email>
2026-08-21 12:36:34 +0200

    Update groupe-e custom component to 2.1.0

--- pkgs/home-assistant-custom-components/groupe-e/default.nix
+++ pkgs/home-assistant-custom-components/groupe-e/default.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent {
   owner = "jocelynthode";
   domain = "groupe_e";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "jocelynthode";
     repo = "hscs-groupe-e";
-    rev = "92cd024dd4319db38a789d3029578ada428c063c";
-    hash = "sha256-5vCb6aR2UX4gBQVj7PMgXbtNORAsmM3ytBhe6HV3fRY=";
+    rev = "ff7c9da1f2d31191c8b0150b92926d2e4f0ab8aa";
+    hash = "sha256-00ToaTEYVSbZSK/xVO1zDb56Axm77iVF6AJlb/Ec1RI=";
   };
 
   dependencies = with python3Packages; [ aiohttp ];
